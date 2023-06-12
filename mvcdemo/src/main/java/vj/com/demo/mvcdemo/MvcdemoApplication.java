a4efc1a6bd7c45fb133bf8049037c0e8758cd501
Vijay Purushothaman <meganj1500@gmail.com>
2023-06-12 13:34:40 +0200

    tomcat config

--- mvcdemo/src/main/java/vj/com/demo/mvcdemo/MvcdemoApplication.java
+++ mvcdemo/src/main/java/vj/com/demo/mvcdemo/MvcdemoApplication.java
@@ -2,12 +2,18 @@ package vj.com.demo.mvcdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class MvcdemoApplication {
+public class MvcdemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcdemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MvcdemoApplication.class);
 	}
 
 }
